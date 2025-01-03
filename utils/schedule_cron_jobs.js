37076dd645be33e76efdcca415ae6c37e4a71a18
factorgh <burchellsbale@gmail.com>
2025-01-03 05:38:19 +0000

    Upate on cron to run 6 am every morning

--- utils/schedule_cron_jobs.js
+++ utils/schedule_cron_jobs.js
@@ -20,149 +20,160 @@ const calculateDailyTotalAccruedReturn = (amount, guaranteedRate) => {
 };
 
 const dailyAccruedReturnJob = () => {
-  cron.schedule("0 0 * * *", async () => {
-    console.log("Starting daily update for accrued returns...");
-
-    try {
-      const currentDate = new Date();
-      const jobName = "dailyAccruedReturn";
-
-      // Log the current time and job name
-      console.log(`Current date: ${currentDate}`);
-      console.log(`Job name: ${jobName}`);
-
-      // Fetch or initialize Job Status
-      let jobStatus = await JobStatus.findOne({ jobName });
-      if (!jobStatus) {
-        console.log("No job status found, initializing...");
-        jobStatus = await JobStatus.create({
-          jobName,
-          lastRun: moment().subtract(1, "days").toDate(),
-        });
-        console.log("Initialized Job Status:", jobStatus);
-      }
-
-      const lastRunDate = jobStatus.lastRun;
-      const daysMissed = moment(currentDate).diff(moment(lastRunDate), "days");
-      console.log(`Last run date: ${lastRunDate}`);
-      console.log(`Days missed: ${daysMissed}`);
-
+  cron.schedule(
+    "0 6 * * *",
+    async () => {
       console.log(
-        "(1)------------------------------JOB STATUS SECTION END---------------------------"
+        `[${moment().format()}] Starting daily update for accrued returns...`
       );
 
-      // Ensure at least one day's calculation occurs
-      const daysToProcess = daysMissed <= 0 ? 1 : daysMissed;
+      try {
+        const currentDate = new Date();
+        const jobName = "dailyAccruedReturn";
 
-      for (let i = 1; i <= daysToProcess; i++) {
-        const targetDate = moment(lastRunDate).add(i, "days").toDate();
-        console.log(`Processing target date: ${targetDate}`);
+        // Log the current time and job name
+        console.log(`Current date: ${currentDate}`);
+        console.log(`Job name: ${jobName}`);
 
-        const investments = await Investment.find({ active: true }).populate(
-          "addOns"
+        // Fetch or initialize Job Status
+        let jobStatus = await JobStatus.findOne({ jobName });
+        if (!jobStatus) {
+          console.log("No job status found, initializing...");
+          jobStatus = await JobStatus.create({
+            jobName,
+            lastRun: moment().subtract(1, "days").toDate(),
+          });
+          console.log("Initialized Job Status:", jobStatus);
+        }
+
+        const lastRunDate = jobStatus.lastRun;
+        const daysMissed = moment(currentDate).diff(
+          moment(lastRunDate),
+          "days"
         );
-        console.log(`Found ${investments.length} active investments`);
+        console.log(`Last run date: ${lastRunDate}`);
+        console.log(`Days missed: ${daysMissed}`);
+
         console.log(
-          "(2)------------------------------ACTIVE INVESTMENTS END---------------------------"
+          "(1)------------------------------JOB STATUS SECTION END---------------------------"
         );
 
-        for (const investment of investments) {
+        // Ensure at least one day's calculation occurs
+        const daysToProcess = daysMissed <= 0 ? 1 : daysMissed;
+
+        for (let i = 1; i <= daysToProcess; i++) {
+          const targetDate = moment(lastRunDate).add(i, "days").toDate();
+          console.log(`Processing target date: ${targetDate}`);
+
+          const investments = await Investment.find({ active: true }).populate(
+            "addOns"
+          );
+          console.log(`Found ${investments.length} active investments`);
           console.log(
-            `(3)Processing investment ID-------------------------------------------------: ${investment._id}`
+            "(2)------------------------------ACTIVE INVESTMENTS END---------------------------"
           );
 
-          try {
-            const quarterEndDate = getQuarterEndDate(investment.startDate);
-            const normalizedTargetDate = moment(targetDate).startOf("day");
-            const normalizedQuarterEndDate =
-              moment(quarterEndDate).startOf("day");
-
-            if (normalizedTargetDate >= normalizedQuarterEndDate) {
-              console.log(`Condition true: Archiving`);
-              console.log(
-                `targetDate: ${normalizedTargetDate}, quarterEndDate: ${normalizedQuarterEndDate}`
-              );
-
-              await archiveTransactions();
-              await rolloverInvestments(investment, quarterEndDate);
-
-              console.log(
-                `Quarter rollover completed for investment ${investment._id}. Continuing updates...`
-              );
-            } else {
-              console.log(`Condition false: Not yet`);
-              console.log(
-                `targetDate: ${normalizedTargetDate}, quarterEndDate: ${normalizedQuarterEndDate}`
-              );
-            }
-            // Calculate principal accrued return
-            const daysForPrincipal = calculateDays(
-              investment.startDate,
-              Math.min(targetDate, quarterEndDate)
-            );
-            console.log(`Days for principal: ${daysForPrincipal}`);
-
-            const principalAccruedReturn = calculateAccruedReturn(
-              investment.principal,
-              investment.guaranteedRate,
-              daysForPrincipal
-            );
+          for (const investment of investments) {
             console.log(
-              `Principal accrued return for investment ID ${investment._id}: ${principalAccruedReturn}`
+              `(3)Processing investment ID-------------------------------------------------: ${investment._id}`
             );
 
-            // const addOnAccruedReturn += investment.addOnAccruedReturn;
+            try {
+              const quarterEndDate = getQuarterEndDate(investment.startDate);
+              const normalizedTargetDate = moment(targetDate).startOf("day");
+              const normalizedQuarterEndDate =
+                moment(quarterEndDate).startOf("day");
 
-            // Calculate add-on accrued return
-            let totalAddOnAccruedReturn = investment.addOnAccruedReturn || 0;
+              if (normalizedTargetDate >= normalizedQuarterEndDate) {
+                console.log(`Condition true: Archiving`);
+                console.log(
+                  `targetDate: ${normalizedTargetDate}, quarterEndDate: ${normalizedQuarterEndDate}`
+                );
 
-            for (const addOn of investment.addOns) {
+                await archiveTransactions();
+                await rolloverInvestments(investment, quarterEndDate);
+
+                console.log(
+                  `Quarter rollover completed for investment ${investment._id}. Continuing updates...`
+                );
+              } else {
+                console.log(`Condition false: Not yet`);
+                console.log(
+                  `targetDate: ${normalizedTargetDate}, quarterEndDate: ${normalizedQuarterEndDate}`
+                );
+              }
+              // Calculate principal accrued return
+              const daysForPrincipal = calculateDays(
+                investment.startDate,
+                Math.min(targetDate, quarterEndDate)
+              );
+              console.log(`Days for principal: ${daysForPrincipal}`);
+
+              const principalAccruedReturn = calculateAccruedReturn(
+                investment.principal,
+                investment.guaranteedRate,
+                daysForPrincipal
+              );
               console.log(
-                `Processing add-on: ${addOn.name} for investment ID: ${investment._id}`
+                `Principal accrued return for investment ID ${investment._id}: ${principalAccruedReturn}`
               );
 
-              // Calculate the daily return for the add-on
-              const dailyAddOnReturn = calculateDailyAddOnAccruedReturn(
-                addOn.amount,
+              // const addOnAccruedReturn += investment.addOnAccruedReturn;
+
+              // Calculate add-on accrued return
+              let totalAddOnAccruedReturn = investment.addOnAccruedReturn || 0;
+
+              for (const addOn of investment.addOns) {
+                console.log(
+                  `Processing add-on: ${addOn.name} for investment ID: ${investment._id}`
+                );
+
+                // Calculate the daily return for the add-on
+                const dailyAddOnReturn = calculateDailyAddOnAccruedReturn(
+                  addOn.amount,
+                  investment.guaranteedRate
+                );
+
+                console.log(
+                  `Calculated daily return for add-on: ${addOn.name}, Amount: ${addOn.amount}, Rate: ${addOn.guaranteedRate}, Daily Return: ${dailyAddOnReturn}`
+                );
+
+                // Accumulate the total add-on accrued return
+                totalAddOnAccruedReturn += dailyAddOnReturn;
+              }
+
+              console.log(
+                `Total add-on accrued return for investment ID ${investment._id}: ${totalAddOnAccruedReturn} `
+              );
+
+              // Update investment
+              investment.totalAccruedReturn += calculateDailyTotalAccruedReturn(
+                investment.principal,
                 investment.guaranteedRate
               );
-
-              console.log(
-                `Calculated daily return for add-on: ${addOn.name}, Amount: ${addOn.amount}, Rate: ${addOn.guaranteedRate}, Daily Return: ${dailyAddOnReturn}`
+              investment.addOnAccruedReturn += totalAddOnAccruedReturn;
+              await investment.save();
+              console.log(`Investment ${investment._id} updated successfully.`);
+            } catch (error) {
+              console.error(
+                `Error processing investment ${investment._id}:`,
+                error
               );
-
-              // Accumulate the total add-on accrued return
-              totalAddOnAccruedReturn += dailyAddOnReturn;
             }
-
-            console.log(
-              `Total add-on accrued return for investment ID ${investment._id}: ${totalAddOnAccruedReturn} `
-            );
-
-            // Update investment
-            investment.totalAccruedReturn += calculateDailyTotalAccruedReturn(
-              investment.principal,
-              investment.guaranteedRate
-            );
-            investment.addOnAccruedReturn += totalAddOnAccruedReturn;
-            await investment.save();
-            console.log(`Investment ${investment._id} updated successfully.`);
-          } catch (error) {
-            console.error(
-              `Error processing investment ${investment._id}:`,
-              error
-            );
           }
         }
-      }
 
-      jobStatus.lastRun = currentDate;
-      await jobStatus.save();
-      console.log("Updated job status last run date.");
-    } catch (error) {
-      console.error("Error during daily update:", error);
+        jobStatus.lastRun = currentDate;
+        await jobStatus.save();
+        console.log("Updated job status last run date.");
+      } catch (error) {
+        console.error("Error during daily update:", error);
+      }
+    },
+    {
+      timezone: "Africa/Accra", // Ensure this matches your desired time zone
     }
-  });
+  );
 };
 
 export default dailyAccruedReturnJob;
